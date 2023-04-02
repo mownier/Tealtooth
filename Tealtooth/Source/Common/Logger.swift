d0ff060e619fab925d07607318f9cd986cb2f336
Mounir Ybanez <rinuom91@gmail.com>
2023-04-02 15:52:08 +0800

    Added operation queue in the DefaultLogger.

--- Tealtooth/Source/Common/Logger.swift
+++ Tealtooth/Source/Common/Logger.swift
@@ -7,13 +7,18 @@ public protocol Logger {
 public class DefaultLogger: Logger {
     private let dateFormatter: DateFormatter
     private let tag: String
+    private let queue: OperationQueue
     public init(tag: String = logTag) {
         self.tag = tag
         self.dateFormatter = DateFormatter()
         self.dateFormatter.dateFormat = "MMM dd, yyyy h:mm:ss a"
+        self.queue = OperationQueue()
+        self.queue.maxConcurrentOperationCount = 1
     }
     public func writeConsole(_ level: Any, _ message: Any) {
-        print("\(dateFormatter.string(from: Date())) [\(tag)] <\(level)> \(message)")
+        queue.addOperation { [unowned self] in
+            print("\(self.dateFormatter.string(from: Date())) [\(self.tag)] <\(level)> \(message)")
+        }
     }
 }
 
