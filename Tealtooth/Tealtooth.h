2a9e70cdfcb1c60b7bd0158b2115b500fa79ce19
Mounir Ybanez <rinuom91@gmail.com>
2023-04-02 15:11:53 +0800

    Init commit.

---
+++ Tealtooth/Tealtooth.h
@@ -0,0 +1,18 @@
+//
+//  Tealtooth.h
+//  Tealtooth
+//
+//  Created by nir on 4/2/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Tealtooth.
+FOUNDATION_EXPORT double TealtoothVersionNumber;
+
+//! Project version string for Tealtooth.
+FOUNDATION_EXPORT const unsigned char TealtoothVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Tealtooth/PublicHeader.h>
+
+
