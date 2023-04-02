0513476f183d9d789c9cecf547418b2a218d192e
Mounir Ybanez <rinuom91@gmail.com>
2023-04-02 15:48:59 +0800

    Added Notification.

---
+++ Tealtooth/Source/Common/Notification.swift
@@ -0,0 +1,22 @@
+import Foundation
+
+public enum TealtoothNotification: String {
+    case onCentralStateUpdated
+    public var name: Notification.Name {
+        return Notification.Name(rawValue: "TealtoothNotification.\(self.rawValue)")
+    }
+}
+
+func postNotification(
+    name: Notification.Name,
+    object: Any? = nil,
+    userInfo: [String: Any]?  = nil
+) {
+    DispatchQueue.main.async {
+        NotificationCenter.default.post(
+            name: name,
+            object: object,
+            userInfo: userInfo
+        )
+    }
+}
