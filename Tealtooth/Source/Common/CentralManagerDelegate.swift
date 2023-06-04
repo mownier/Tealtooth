a1e42030e31b01755ce434241e8b400cd33ea5ed
Mounir Ybanez <rinuom91@gmail.com>
2023-06-04 13:39:56 +0800

    Configured to not process connect result and semaphone if bluetooth assistant did not initiate connect.

--- Tealtooth/Source/Common/CentralManagerDelegate.swift
+++ Tealtooth/Source/Common/CentralManagerDelegate.swift
@@ -19,10 +19,16 @@ class CentralManagerDelegate: NSObject, CBCentralManagerDelegate {
         )
     }
     func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        if bluetoothAssistant?.didInitiateConnect == false {
+            return
+        }
         bluetoothAssistant?.connectResult = .success(Peripheral(proxy: peripheral))
         bluetoothAssistant?.semaphore.signal()
     }
     func centralManager(_ central: CBCentralManager, didFailToConnect peripheral: CBPeripheral, error: Error?) {
+        if bluetoothAssistant?.didInitiateConnect == false {
+            return
+        }
         bluetoothAssistant?.connectResult = .failure(error ?? TealtoothError.errorNotDetermined)
         bluetoothAssistant?.semaphore.signal()
     }
