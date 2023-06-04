763ae9b4c9bca02ecd1150d3bee0ec319d4c0a14
Mounir Ybanez <rinuom91@gmail.com>
2023-06-04 13:23:14 +0800

    Implemented disconnect function.

--- Tealtooth/Source/Common/BluetoothAssistant.swift
+++ Tealtooth/Source/Common/BluetoothAssistant.swift
@@ -5,6 +5,7 @@ public class BluetoothAssistant {
     private var centralManagerDelegate: CentralManagerDelegate
     private(set) var semaphore: DispatchSemaphore
     var connectResult: Result<Peripheral, Swift.Error>?
+    var disconnectResult: Result<Peripheral, Swift.Error>?
     public init(
         queue: DispatchQueue? = nil,
         options: [String: Any]? = nil
@@ -84,8 +85,34 @@ public class BluetoothAssistant {
     }
     @discardableResult
     public func disconnect(
-        _ peripheral: Peripheral
+        _ peripheral: Peripheral,
+        timeout: Double
     ) -> Result<Peripheral, Swift.Error> {
-        return .failure(TealtoothError.unimplemented)
+        if centralManager.state != .poweredOn {
+            let error = TealtoothError.bluetoothNotPoweredOn
+            logger?.writeConsole(LogLevel.error, "on disconnect, an error occurred \(error)")
+            return .failure(error)
+        }
+        if peripheral.proxy.state == .disconnected {
+            logger?.writeConsole(LogLevel.info, "on disconnect, it seems that the peripheral is already disconnected")
+            return .success(peripheral)
+        }
+        if peripheral.proxy.state == .disconnecting {
+            let error = TealtoothError.stillDisconnecting
+            logger?.writeConsole(LogLevel.error, "on disconnect, an error occurred \(error)")
+            return .failure(error)
+        }
+        logger?.writeConsole(LogLevel.info, "on disconnect, timeout = \(timeout)")
+        centralManager.cancelPeripheralConnection(peripheral.proxy)
+        let semaphoreResult = semaphore.wait(timeout: .now() + timeout)
+        if semaphoreResult == .timedOut {
+            let error = TealtoothError.timedOutWhileTryingToDisconnect
+            logger?.writeConsole(LogLevel.error, "on disconnect, an error occurred \(error)")
+            return .failure(error)
+        }
+        let result = disconnectResult ?? .failure(TealtoothError.disconnectResultIsNil)
+        disconnectResult = nil
+        logger?.writeConsole(LogLevel.error, "on disconnect, result = \(result)")
+        return result
     }
 }
