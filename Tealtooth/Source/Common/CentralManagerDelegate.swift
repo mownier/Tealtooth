9653c58e27130789e5162e01a6122110e71a3acc
Mounir Ybanez <rinuom91@gmail.com>
2023-06-04 13:37:02 +0800

    Configured what to do the peripheral is disconnected.

--- Tealtooth/Source/Common/CentralManagerDelegate.swift
+++ Tealtooth/Source/Common/CentralManagerDelegate.swift
@@ -26,4 +26,22 @@ class CentralManagerDelegate: NSObject, CBCentralManagerDelegate {
         bluetoothAssistant?.connectResult = .failure(error ?? TealtoothError.errorNotDetermined)
         bluetoothAssistant?.semaphore.signal()
     }
+    func centralManager(_ central: CBCentralManager, didDisconnectPeripheral peripheral: CBPeripheral, error: Error?) {
+        if bluetoothAssistant?.didInitiateDisconnect == false {
+            postNotification(
+                name: TealtoothNotification.onDisconnectedUnexpectedly.name,
+                object: UnexpectedDisconnection(
+                    peripheral: Peripheral(proxy: peripheral),
+                    error: error
+                )
+            )
+            return
+        }
+        if let err = error {
+            bluetoothAssistant?.disconnectResult = .failure(err)
+            bluetoothAssistant?.semaphore.signal()
+        }
+        bluetoothAssistant?.disconnectResult = .success(Peripheral(proxy: peripheral))
+        bluetoothAssistant?.semaphore.signal()
+    }
 }
