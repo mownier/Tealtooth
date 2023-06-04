345d6ba97650774df7931122cefe679d7192716c
Mounir Ybanez <rinuom91@gmail.com>
2023-06-04 11:48:11 +0800

    Moved CentralManagerDelegate and added posting notificatin on discovered peripheral.

---
+++ Tealtooth/Source/Common/CentralManagerDelegate.swift
@@ -0,0 +1,20 @@
+import CoreBluetooth
+
+class CentralManagerDelegate: NSObject, CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        postNotification(
+            name: TealtoothNotification.onCentralStateUpdated.name,
+            object: central.state
+        )
+    }
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        postNotification(
+            name: TealtoothNotification.onDiscoveredPeripheral.name,
+            object: Advertiser(
+                peripheral: Peripheral(proxy: peripheral),
+                info: advertisementData,
+                rssi: RSSI.intValue
+            )
+        )
+    }
+}
