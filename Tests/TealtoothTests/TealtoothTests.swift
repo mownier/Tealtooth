95cd6333d6c7c3e20cf4b95abaa8feba30ed288e
Mounir Ybanez <rinuom91@gmail.com>
2023-09-02 10:54:38 +0800

    Reorganized for SPM.

---
+++ Tests/TealtoothTests/TealtoothTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import Tealtooth
+
+final class TealtoothTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertEqual("Hello, World!", "Hello, World!")
+    }
+}
